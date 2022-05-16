d70b001e22278c94abc41214f124a833db612614
philwang-1990 <phil.wang@kkday.com>
2022-05-16 11:19:10 +0800

    ADD-OCBT.DataModel

---
+++ KKday.Web.OCBT/Models/Model/DataModel/BookingLogModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace KKday.Web.OCBT.Models.Model.DataModel
+{
+    public class BookingLogModel
+    {
+        public int booking_log_xid { get; set; }        // log檔xid
+        public int booking_mst_xid { get; set; }        // 主檔xid
+        public int booking_dtl_xid { get; set; }        // 明細檔xid
+        public string booking_source { get; set; }      // 子單sku_oid
+        public string status { get; set; }              // 狀態
+        public string create_user { get; set; }
+        public DateTime create_datetime { get; set; }
+        public string modify_user { get; set; }
+        public DateTime modify_datetime { get; set; }
+    }
+}
---
+++ KKday.Web.OCBT/Models/Model/DataModel/BookingMstModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KKday.Web.OCBT.Models.Model.DataModel
+{
+    public class BookingMstModel
+    {
+        public int booking_mst_xid { get; set; } // 主檔
+        public string order_mid { get; set; }
+        public int order_oid { get; set; }
+        public int prod_oid { get; set; }
+        public Dictionary<string, string> booking_model { get; set; }
+        public Dictionary<string, string> product_model { get; set; }
+        public string booking_mst_status { get; set; }
+        public int voucher_deadline { get; set; }
+        public string create_user { get; set; }
+        public DateTime create_datetime { get; set; }
+        public string modify_user { get; set; }
+        public DateTime modify_datetime { get; set; }
+    }
+}
