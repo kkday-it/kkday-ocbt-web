36e2462faabd571e3f64781d8f1de480318991d1
Young.Chen <Young.Chen@kkday.com>
2022-05-24 11:27:29 +0800

    Add RedisHelper redis push and pop

---
+++ KKday.Web.OCBT/AppCode/RedisHelper.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using StackExchange.Redis;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using KKday.Web.OCBT.AppCode;
+
+namespace KKday.Web.B2D.N3.AppCode
+{
+    public interface IRedisHelper
+    {
+        void SetRedis(string obj, string redisKey, int expireMinute);
+
+        string getRedis(string redisKey);
+
+        void delRedis(string guidKey, string redisKey);
+
+        void Push(RedisKey stackName, RedisValue value);
+
+        RedisValue Pop(RedisKey stackName);
+
+        //IRedisHelper reChkConnect();
+        ConnectionMultiplexer reChkConnect();
+    }
+
+
+    public class RedisHelper : IRedisHelper
+    {
+        public ConnectionMultiplexer kkrds { get; set; }
+        public string redisConnectStr { get; private set; }
+
+        public RedisHelper(IConfiguration configuration)
+        {
+            //redis 
+            this.redisConnectStr = configuration["Redis:kkday"];
+            kkrds = ConnectionMultiplexer.Connect(redisConnectStr);
+        }
+
+        //存到redis
+        public void SetRedis(string obj, string redisKey, int expireMinute)
+        {
+            try
+            {
+                //kkredis  
+                this.kkrds = reChkConnect();
+                IDatabase db = kkrds.GetDatabase();
+                db.StringSet(redisKey, obj, TimeSpan.FromMinutes(expireMinute));
+            }
+            catch (Exception ex)
+            {
+                Website.Instance.logger.Fatal($"setRedis_error:" + ex.Message.ToString());
+            }
+        }
+        public void delRedis(string guidKey, string redisKey)
+        {
+            try
+            {
+                //kkredis  
+                this.kkrds = reChkConnect();
+                IDatabase db = kkrds.GetDatabase();
+                db.KeyDelete(redisKey);
+
+                //刷新redis server 
+                Clear();
+            }
+            catch (Exception ex)
+            {
+                Website.Instance.logger.Fatal($"guidKey : {guidKey}; delRedis_error:" + ex.Message.ToString());
+            }
+        }
+        public void Clear()
+        {
+            var endpoints = kkrds.GetEndPoints(true);
+            foreach (var endpoint in endpoints)
+            {
+                var server = kkrds.GetServer(endpoint);
+                server.FlushAllDatabases();
+            }
+        }
+        public string getRedis(string redisKey)
+        {
+            try
+            {
+                //kkredis  
+                this.kkrds = reChkConnect();
+                IDatabase db = kkrds.GetDatabase();
+
+                string obj = db.StringGet(redisKey);
+                return obj;
+            }
+            catch (Exception ex)
+            {
+                Website.Instance.logger.Fatal($"getRedis_error:" + ex.Message.ToString());
+                return null;
+            }
+        }
+
+        public ConnectionMultiplexer reChkConnect()
+        {
+            if (kkrds == null)
+            {
+                kkrds = ConnectionMultiplexer.Connect(this.redisConnectStr);
+            }
+            return kkrds;
+        }
+
+        public void Push(RedisKey stackName, RedisValue value)
+        {
+            this.kkrds = reChkConnect();
+            this.kkrds.GetDatabase().ListRightPush(stackName, value);
+        }
+        public RedisValue Pop(RedisKey stackName)
+        {
+            this.kkrds = reChkConnect();
+            return this.kkrds.GetDatabase().ListLeftPop(stackName);
+        }
+    }
+}
---
+++ KKday.Web.OCBT/Area/V1/ComboBookingController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KKday.Web.B2D.N3.AppCode;
+using KKday.Web.OCBT.Models.Model.DataModel;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace KKday.Web.OCBT.Area.V1
+{
+    [Route("api/v1/[controller]")]
+    public class ComboBookingController : Controller
+    {
+        private IRedisHelper _redisHelper;
+        public ComboBookingController(IRedisHelper redisHelper)
+        {
+            _redisHelper = redisHelper;
+        }
+        // GET: api/values
+        [HttpPost("CrtOrder")]
+        public ResponseJavaModel CrtOrder()
+        {
+            _redisHelper.Push("CrtOrder", "");
+            return new ResponseJavaModel {
+                metadata=new ResponseMetaModel {
+                    status= "1000",
+                    description="正確無誤"
+                }
+            };
+        }
+
+    }
+}
